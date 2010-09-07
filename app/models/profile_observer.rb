fc115496c7062ed907bc4a450dd77da23c2024e2
jason gagne <jgagne33@gmail.com>
2010-09-07 14:10:33 -0700

    making profile observer descend from person observer

--- app/models/profile_observer.rb
+++ app/models/profile_observer.rb
@@ -1,4 +1,4 @@
-class ProfileObserver < PersonObserver
+class ProfileObserver < ActiveRecord::Observer
   def after_create(profile)
     if CMS_CONFIG['site_settings']['member_confirmation'] && profile.person && profile.person.created_at > 5.minutes.ago
       PersonMailer.deliver_confirmation_to_admin(profile) 
